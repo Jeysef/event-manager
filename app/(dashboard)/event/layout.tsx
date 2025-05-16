079f3b8d042085e93a60d0770830c39316bcffcc
Jeysef <josimich7@gmail.com>
2025-05-16 18:31:13 +0200

    feat: add breadcrumb to events

---
+++ app/(dashboard)/event/layout.tsx
@@ -0,0 +1,45 @@
+"use client"
+import React from 'react'
+import { usePathname } from 'next/navigation'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb"
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const pathname = usePathname()
+  const segments = pathname.split('/').filter(Boolean)
+  return (
+    <div className="flex flex-col gap-4 p-4">
+      <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+        <BreadcrumbLink href="/">Home</BreadcrumbLink>
+        </BreadcrumbItem>
+        {segments.map((segment, idx) => {
+        const href = '/' + segments.slice(0, idx + 1).join('/');
+        const isLast = idx === segments.length - 1;
+        return (
+          <React.Fragment key={href}>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            {isLast ? (
+            <BreadcrumbPage>
+              {segment.charAt(0).toUpperCase() + segment.slice(1)}
+            </BreadcrumbPage>
+            ) : (
+            <BreadcrumbLink href={href}>
+              {segment.charAt(0).toUpperCase() + segment.slice(1)}
+            </BreadcrumbLink>
+            )}
+          </BreadcrumbItem>
+          </React.Fragment>
+        );
+        })}
+      </BreadcrumbList>
+      </Breadcrumb>
+      {children}
+    </div>
+  )
+}
