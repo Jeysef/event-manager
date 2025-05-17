04f946dffa07b75d5ccef97d6f065c50cd030287
Jeysef <josimich7@gmail.com>
2025-05-17 14:53:57 +0200

    fix: correct breadcrumb's links

--- app/(dashboard)/event/layout.tsx
+++ app/(dashboard)/event/layout.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { usePathname } from 'next/navigation'
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb"
+import Link from 'next/link';
 
 export default function Layout({
   children,
@@ -13,31 +14,33 @@ export default function Layout({
   return (
     <div className="flex flex-col gap-4 p-4">
       <Breadcrumb>
-      <BreadcrumbList>
-        <BreadcrumbItem>
-        <BreadcrumbLink href="/">Home</BreadcrumbLink>
-        </BreadcrumbItem>
-        {segments.map((segment, idx) => {
-        const href = '/' + segments.slice(0, idx + 1).join('/');
-        const isLast = idx === segments.length - 1;
-        return (
-          <React.Fragment key={href}>
-          <BreadcrumbSeparator />
+        <BreadcrumbList>
           <BreadcrumbItem>
-            {isLast ? (
-            <BreadcrumbPage>
-              {segment.charAt(0).toUpperCase() + segment.slice(1)}
-            </BreadcrumbPage>
-            ) : (
-            <BreadcrumbLink href={href}>
-              {segment.charAt(0).toUpperCase() + segment.slice(1)}
+            <BreadcrumbLink href="/" asChild>
+              <Link href="/">Home</Link>
             </BreadcrumbLink>
-            )}
           </BreadcrumbItem>
-          </React.Fragment>
-        );
-        })}
-      </BreadcrumbList>
+          {segments.map((segment, idx) => {
+            const href = '/' + segments.slice(0, idx + 1).join('/');
+            const isLast = idx === segments.length - 1;
+            return (
+              <React.Fragment key={href}>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                  {isLast ? (
+                    <BreadcrumbPage>
+                      {segment.charAt(0).toUpperCase() + segment.slice(1)}
+                    </BreadcrumbPage>
+                  ) : (
+                    <BreadcrumbLink href={href} asChild>
+                      <Link href={href}>{segment.charAt(0).toUpperCase() + segment.slice(1)}</Link>
+                    </BreadcrumbLink>
+                  )}
+                </BreadcrumbItem>
+              </React.Fragment>
+            );
+          })}
+        </BreadcrumbList>
       </Breadcrumb>
       {children}
     </div>
